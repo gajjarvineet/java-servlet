446ac522f70aba9f8896f6ff1dabbfbaa6e3fb89
Vinit Gajjar <gajjarvineet@hotmail.com>
2021-02-23 13:57:58 +0530

    first commit

---
+++ src/java/Power.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author gajja
+ */
+public class Power extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Multiplication Table</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Square Number</h1>");
+            out.println("<form method=\"GET\">");
+            out.println("Base: <input type=\"number\" name=\"num\"/></br></br>");
+            out.println("Power: <input type=\"number\" name=\"pow\"/></br></br>");
+            out.println("<input type=\"submit\" value=\"Submit\"/>");
+            out.println("</form></br></br>");
+            if(request.getParameter("num")!=null && request.getParameter("pow")!=null){
+                
+                int num = Integer.parseInt(request.getParameter("num"));
+                int pow = Integer.parseInt(request.getParameter("pow"));
+                
+                int ans = num;
+                for(int i = 1; i<pow; i++)
+                    ans *= num;
+                
+                out.println("<span>"+num+" ^ "+pow+" = "+ans+"</span>");
+
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+        catch(Exception e){
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
